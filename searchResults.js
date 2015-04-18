27599a53c953f66b057fa5fa97fc1fdb79f080d7
Joshua Raichur <hello@joshuaraichur.com>
2015-04-19 00:00:50 +0530

    list formatting

--- searchResults.js
+++ searchResults.js
@@ -12,6 +12,35 @@ var {
   Component
 } = React;
 
+var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  seperator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
+
+
 class SearchResults extends Component {
 
   constructor(props) {
@@ -23,14 +52,24 @@ class SearchResults extends Component {
       };
   }
 
-  renderRow(rowData, sectionId, rowId) {
+  renderRow(rowData, sectionId, rowID) {
+    var price = rowData.price_formatted.split(' ')[0];
+
     return (
-      <TouchableHighlight
+      <TouchableHighlight onPress={() => this.rowPressed(rowData.guid)}
         underlayColor='#dddddd'>
         <View>
-          <Text>{rowData.title}</Text>
+          <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{ uri: rowData.img_url }} />
+            <View style={styles.textContainer}>
+              <Text style={styles.price}>£{price}</Text>
+              <Text style={styles.title}
+                    numberOfLines={1}>{rowData.title}</Text>
+            </View>
+          </View>
+          <View style={styles.separator}/>
         </View>
-        </TouchableHighlight>
+      </TouchableHighlight>
     );
   }
 
