7431f893b60eed280ff1b9d4d6712e3f425e07ae
Joshua Raichur <hello@joshuaraichur.com>
2015-04-18 23:44:10 +0530

    listview

--- searchResults.js
+++ searchResults.js
@@ -11,3 +11,35 @@ var {
   Image,
   Component
 } = React;
+
+class SearchResults extends Component {
+
+  constructor(props) {
+    super(props);
+    var dataSource = new ListView.DataSource(
+      {rowHasChanged: (r1,r2) => r1.guid !== r2.guid});
+      this.state = {
+        dataSource: dataSource.cloneWithRows(this.props.listings)
+      };
+  }
+
+  renderRow(rowData, sectionId, rowId) {
+    return (
+      <TouchableHighlight
+        underlayColor='#dddddd'>
+        <View>
+          <Text>{rowData.title}</Text>
+        </View>
+        </TouchableHighlight>
+    );
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow.bind(this)}/>
+    );
+  }
+
+}
