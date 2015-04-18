322e343af6f97f5944d67fb3ed3f4591d477314d
Joshua Raichur <hello@joshuaraichur.com>
2015-04-18 22:30:51 +0530

    add searchPage.js

---
+++ SearchPage.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableHighlight,
+  ActivityIndicatorIOS,
+  Image,
+  Component
+} = React;
